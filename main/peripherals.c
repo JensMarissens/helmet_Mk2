b6d4882ebbfc8dff705ff660f63c1d17558d4238
Jens Marissens <marissens.jens@hotmail.com>
2024-06-15 23:58:12 +0200

    Just setting up more Modular coding

---
+++ main/peripherals.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "peripherals.h"
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
---
+++ main/peripherals.h
@@ -0,0 +1,9 @@
+#ifndef PERIPHERALS_H
+#define PERIPHERALS_H
+
+void matrix_display(void);
+void matrix_power_down(void);
+
+void matrix_power_on(void);
+
+#endif
