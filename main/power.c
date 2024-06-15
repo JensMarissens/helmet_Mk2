33c9e9080fc3ab1a40ef053e2c3ff0e8614fdece
Jens Marissens <marissens.jens@hotmail.com>
2024-06-15 16:31:36 +0200

    Added syntax to see what core I'm running on.

--- main/power.c
+++ main/power.c
@@ -6,7 +6,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-static const char *TAG = "LED_blink";
+static const char *TAG = "Active Core";
 static const BaseType_t app_cpu = 0;
 
 static TaskHandle_t task_handle_1 = NULL;
@@ -19,7 +19,7 @@ void task_1(void *parameters)
 
     while (1)
     {
-        ESP_LOGI(TAG, "Task 1 is running");
+        ESP_LOGI("Task 1", "running on Core %d", xPortGetCoreID());
         gpio_set_level(GPIO_NUM_2, 1);
         vTaskDelay(500 / portTICK_PERIOD_MS); // This has to exist to relinquish control back to the scheduler.
         gpio_set_level(GPIO_NUM_2, 0);
@@ -32,15 +32,17 @@ void task_2()
 
     for (;;)
     {
-        ESP_LOGI("Multitasking", "Task 2 is running");
+        ESP_LOGI("Task 1", "running on Core %d", xPortGetCoreID());
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
 
 void power_on(void)
 {
-    ESP_LOGI(TAG, "Booting");
+    ESP_LOGI("power_on", "Booting");
 
     xTaskCreatePinnedToCore(task_1, "Task1", 2048, NULL, 1, &task_handle_1, app_cpu);
     xTaskCreatePinnedToCore(task_2, "Task2", 2048, NULL, 1, &task_handle_2, app_cpu);
+    //xTaskCreate(task_1, "Task1", 2048, NULL, 1, &task_handle_1);
+    //xTaskCreate(task_2, "Task2", 2048, NULL, 1, &task_handle_2);
 }
