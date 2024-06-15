60cf98ea1d2d6d29d55a4f5baf5cd521c4c71d6b
Jens Marissens <marissens.jens@hotmail.com>
2024-06-15 15:01:58 +0200

    Okay so it doesn't work, but it *has* stopped crashing. So it also doesn't not work..

--- main/main.c
+++ main/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "power.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 // power modes and management
 // alphanumericToBitmap library
 // display animations
@@ -8,4 +11,5 @@
 void app_main(void)
 {
     power_on();
+    vTaskDelay(100/portTICK_PERIOD_MS);
 }
--- main/power.c
+++ main/power.c
@@ -2,12 +2,37 @@
 #include "power.h"
 #include "esp_log.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 static const char* TAG = "power";
+static const BaseType_t app_cpu = 1;
+
+static TaskHandle_t task_handle_1 = NULL;
+
+
+void task_1(void *parameters){
+
+    /*
+    while(1){
+        ESP_LOGI(TAG, "Task 1 is running");
+        vTaskDelay(20/portTICK_PERIOD_MS);
+    }*/
+   vTaskDelay(100/portTICK_PERIOD_MS);
+}
 
 void power_on(void) {
     ESP_LOGI(TAG, "Booting");
 
+    xTaskCreatePinnedToCore(task_1, "Task1", 1024, NULL, 2, &task_handle_1, app_cpu);
 
+    vTaskDelay(100/portTICK_PERIOD_MS);
+    
 
+    
+    if( task_handle_1 != NULL )
+    {
+        vTaskDelete( task_handle_1 );
+    }
+    
 }
